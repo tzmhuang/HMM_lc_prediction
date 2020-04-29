68c4a7c0d1804f847a4bc5c2cf4fec2e6684535a
Tom Huang <huangtom@Tom-Huang.local>
2020-04-30 02:12:51 +0800

    HMM: added multiple observation sequence support adn bug fixes

--- training/HiddenMarkovModel.py
+++ training/HiddenMarkovModel.py
@@ -19,6 +19,8 @@ class HiddenMarkovModel(object):
         self.random_state = random_state
         self.n_iter = n_iter
         self.thresh = thresh
+        self._convergence = []
+        self._converged = False
         if method == 'gmm':
             self.method = method
             self.gmm_k = gmm_k
@@ -32,7 +34,7 @@ class HiddenMarkovModel(object):
         n_mix = self.gmm_k
         n_obs, n_dim = X.shape
         if self.method == 'gmm':
-            self.start_prob = np.ones(n_dim)/n_dim
+            self.start_prob = np.ones(n_sts)/n_sts
             self.A = np.ones((n_sts, n_sts))/n_sts
             self.B = []
             self.weights = np.zeros((n_sts, n_mix))
@@ -61,8 +63,9 @@ class HiddenMarkovModel(object):
             return
         return
 
-    def fit(self, X, n_iter):
+    def fit(self, X, seq_length=[], n_iter=2):
         X = check_array(X)
+        n_seq = max(len(seq_length), 1)
         n_sts = len(self.states)
         n_obs, n_dim = X.shape
         n_mix = self.gmm_k
@@ -73,35 +76,73 @@ class HiddenMarkovModel(object):
                 raise ValueError("Problem with initialization")
         if self.method == 'gmm':
             for i in range(n_iter):
-                emitlogprob = self._log_emission(X)
+                start_prob_accum = np.zeros(n_sts)
+                xisum_accum = np.zeros((n_sts, n_sts))
+                gamma_sum_accum = np.zeros((n_sts, n_mix))
+                means_num_accum = np.zeros((n_sts, n_mix, n_dim))
+                sigmas_num_accum = np.zeros((n_sts, n_mix, n_dim, n_dim))
                 logA = np.log(self.A)
-                fw = self._pass_forward(emitlogprob, logA)
-                bw = self._pass_backward(emitlogprob, logA)
-                # M-step update
-                self.start_prob = np.exp(fw[0] + bw[0])
+                obs_logprob_accum = 0
+                print('in loop 1', i)
+                for j in range(n_seq):
+                    print('in loop 2 ', j)
+                    S = self._get_seq(X, seq_length, j)
+                    emitlogprob = self._log_emission(S)
+                    fw = self._pass_forward(emitlogprob, logA)
+                    bw = self._pass_backward(emitlogprob, logA)
+                    obs_logprob_accum += self._precision_lse(fw[0], axis=0)
+                    print('fw: \n', fw)
+                    print('bw: \n', bw)
+                    print('framelogprob: \n', emitlogprob)
+                    # accumulate start prob
+                    temp = fw[0] + bw[0]
+                    self._log_normalize(temp, axis=0)
+                    start_prob_accum += np.exp(temp)
+                    # accumulate logxisum
+                    logxisum = self._log_sum_xi(
+                        S, fw, bw, emitlogprob, logA)
+                    print('logxisum: \n', logxisum)
+                    xisum_accum += np.exp(logxisum)
+                    # accumulate gamma_sum
+                    gamma = np.exp(self._log_gamma(S, fw, bw))
+                    print('gamma; \n', gamma)
+                    gamma_sum_accum += gamma.sum(axis=0)
+                    # accumulate means
+                    means_num_accum += np.einsum('jik,jh->ikh', gamma, S)
+                    # accumulate sigma
+                    d = S[:, None, None, :] - self.means[None, :, :, :]
+                    sigmas_num_accum += np.sum(
+                        gamma[:, :, :, None, None]*(d[:, :, :, :, None]*d[:, :, :, None, :]), axis=0)
+                # update start_prob
+                prev_start_prob = self.start_prob.copy()
+                self.start_prob = start_prob_accum
                 self._normalize(self.start_prob, axis=0)
-                # Update A
-                logxisum = self._log_sum_xi(X, fw, bw, emitlogprob, logA)
-                xi = np.exp(logxisum)
-                new_A = xi.copy()
+                print('update startprob: ', prev_start_prob,
+                      '->', self.start_prob)
+                # update A
+                new_A = xisum_accum
                 self._normalize(new_A, axis=1)
-                gamma = np.exp(self._log_gamma(X, fw, bw))
-                gamma_sum = gamma.sum(axis=0)
-                # Update Weights
-                new_weights = gamma_sum.copy()
+                # update weights
+                new_weights = gamma_sum_accum.copy()
+                print('new_weights num\n', new_weights)
                 self._normalize(new_weights, axis=1)
-                # Update Mean
-                means_num = np.einsum('jik,jh->ikh', gamma, X)
-                means_den = gamma_sum[:, :, None]
-                new_means = means_num/means_den
-                # Update Sigma
-                d = X[:, None, None, :] - self.means[None, :, :, :]
-                sigmas_num = np.sum(
-                    gamma[:, :, :, None, None]*(d[:, :, :, :, None]*d[:, :, :, None, :]), axis=0)
-                sigmas_den = gamma_sum[:, :, None, None]
-                new_sigmas = sigmas_num/sigmas_den
+                # update mean
+                means_den = gamma_sum_accum[:, :, None]
+                new_means = means_num_accum/means_den
+                print('new_means num\n', means_num_accum)
+                # update sigma
+                sigmas_den = gamma_sum_accum[:, :, None, None]
+                new_sigmas = sigmas_num_accum/sigmas_den
+                print('new_covs num\n', sigmas_num_accum)
+                # update parameter
                 self._update_param(
                     {'A': new_A, 'C': new_weights, 'MU': new_means, 'SIGMA': new_sigmas})
+                # update Convergence
+                self._update_convergence(obs_logprob_accum)
+                if self._converged:
+                    print('Done')
+                    print('Convergence: \n', self._convergence)
+                    break
         else:
             raise ValueError(
                 "\"{0}\" method not supported".format(self.method))
@@ -110,6 +151,30 @@ class HiddenMarkovModel(object):
 
     def predict(self):
         pass
+
+    def _get_seq(self, X, seq_length, seq_id):
+        pl = 0
+        if len(seq_length) < 1:
+            cl = len(X)
+        else:
+            cl = seq_length[seq_id]
+        if seq_id > 0:
+            pl = sum(seq_length[:seq_id])
+            cl += pl
+        return X[pl:cl]
+
+    def _update_convergence(self, logprob):
+        self._convergence.append(logprob)
+        if len(self._convergence) < 2:
+            return
+        delta_logprob = self._convergence[-1] - self._convergence[-2]
+        if delta_logprob < 0:
+            self._converged = True
+            print("Convergence Error: EM iteration diverged")
+        else:
+            if delta_logprob < self.thresh:
+                self._converged = True
+        return
 
     def _update_param(self, param):
         if self.method == "gmm":
@@ -131,43 +196,43 @@ class HiddenMarkovModel(object):
         for s in self.states:
             lge = self.B[s].log_eval(obs)
             lgc = np.log(self.B[s].c)
-            log_emission[:, s] = self._lse(lge + lgc, axis=1)
+            log_emission[:, s] = self._precision_lse(lge + lgc, axis=1)
         return log_emission
 
     def _pass_forward(self, emitlogprob, logA):
-        n_obs, n_dim = emitlogprob.shape
-        fwlogprob = np.zeros((n_obs, n_dim))
-        temp = np.zeros(n_dim)
+        n_obs, n_sts = emitlogprob.shape
+        fwlogprob = np.zeros((n_obs, n_sts))
+        temp = np.zeros(n_sts)
         for t in range(n_obs):
             if t == 0:
                 fwlogprob[t] = emitlogprob[t] + np.log(self.start_prob)
             else:
-                for s in range(n_dim):
-                    for r in range(n_dim):
+                for s in range(n_sts):
+                    for r in range(n_sts):
                         temp[r] = fwlogprob[t-1, r] + logA[r, s]
                     fwlogprob[t, s] = self._precision_lse(
                         temp) + emitlogprob[t, s]
         return fwlogprob
 
     def _pass_backward(self, emitlogprob, logA):
-        n_obs, n_dim = emitlogprob.shape
-        bwlogprob = np.zeros((n_obs, n_dim))
+        n_obs, n_sts = emitlogprob.shape
+        bwlogprob = np.zeros((n_obs, n_sts))
         for t in range(n_obs-2, -1, -1):
             bwlogprob[t] = self._precision_lse(
                 bwlogprob[t+1]+emitlogprob[t+1]+logA, axis=1)
         return bwlogprob
 
     def _viterbi(self, emitlogprob, logA):
-        n_obs, n_dim = emitlogprob.shape
-        viterbi_prob = np.zeros((n_obs, n_dim))
-        viberbi_bt = np.zeros((n_obs, n_dim))
+        n_obs, n_sts = emitlogprob.shape
+        viterbi_prob = np.zeros((n_obs, n_sts))
+        viberbi_bt = np.zeros((n_obs, n_sts))
         viterbi_path = np.zeros(n_obs)
-        temp = np.zeros(n_dim)
+        temp = np.zeros(n_sts)
         for t in range(n_obs):
             if t == 0:
                 viterbi_prob[t] = emitlogprob[t]+np.log(self.start_prob)
             else:
-                for s in range(n_dim):
+                for s in range(n_sts):
                     temp = viterbi_prob[t-1]+logA[:, s]+emitlogprob[t]
                     viterbi_prob[t, s] = np.max(temp)
                     viberbi_bt[t, s] = np.argmax(temp)
@@ -179,23 +244,25 @@ class HiddenMarkovModel(object):
         return (viterbi_score, viterbi_path)
 
     def _log_sum_xi(self, obs, fwlogprob, bwlogprob, emitlogprob, logA):
+        n_sts = len(self.states)
         n_obs, n_dim = obs.shape
-        logsumxi = np.zeros((n_dim, n_dim))
-        for i in range(n_dim):
-            for j in range(n_dim):
-                logsumxi[i, j] = self._lse(
+        logsumxi = np.zeros((n_sts, n_sts))
+        for i in range(n_sts):
+            for j in range(n_sts):
+                logsumxi[i, j] = self._precision_lse(
                     fwlogprob[:n_obs-1, i] +
                     bwlogprob[1:, j]+emitlogprob[1:, j])+logA[i, j]
-        den = self._lse(fwlogprob[-1])
+        den = self._precision_lse(fwlogprob[-1])
         return logsumxi-den
 
     def _log_gamma(self, obs, fwlogprob, bwlogprob):
+        n_sts = len(self.states)
         n_obs, n_dim = obs.shape
         n_mix = self.gmm_k
         logprob = fwlogprob + bwlogprob
-        loggamma = np.zeros((n_obs, n_dim, n_mix))
-        den = self._lse(fwlogprob[0]+bwlogprob[0])
-        for s in range(n_dim):
+        loggamma = np.zeros((n_obs, n_sts, n_mix))
+        den = self._precision_lse(fwlogprob[0]+bwlogprob[0])
+        for s in range(n_sts):
             logemit = self.B[s].log_eval(obs)+np.log(self.B[s].c)
             self._log_normalize(logemit, axis=1)
             loggamma[:, s, :] = logprob[:, s, np.newaxis] + logemit
@@ -206,8 +273,8 @@ class HiddenMarkovModel(object):
 
     def _precision_lse(self, a, axis=None):
         temp = np.asarray(a, dtype=np.longdouble)
-        max = np.max(a, axis=axis)
-        return np.log(np.sum(np.exp(temp), axis=axis)/np.exp(max)) + max
+        max = np.max(a, axis=axis, keepdims=True)
+        return np.log(np.sum(np.exp(temp-max), axis=axis)) + max.squeeze(axis=axis)
 
     def _normalize(self, a, axis=None):
         sum = np.sum(a, axis=axis)
@@ -220,7 +287,7 @@ class HiddenMarkovModel(object):
         a /= sum
 
     def _log_normalize(self, a, axis=None):
-        sum = self._lse(a, axis=axis)
+        sum = self._precision_lse(a, axis=axis)
         if axis > 0:
             s = list(a.shape)
             s[axis] = 1
@@ -241,7 +308,7 @@ class HiddenMarkovModel(object):
             meta = {}
             meta['method'] = self.method
             meta['states'] = self.states
-            meta['n_dim'] = len(self.A)
+            meta['n_dim'] = self.means.shape[2]
             meta['gmm_k'] = self.gmm_k
             param = {}
             param['A'] = self.A.tolist()
@@ -277,7 +344,7 @@ class HiddenMarkovModel(object):
             self.states = meta['states']
             self.method = meta['method']
             self.gmm_k = meta['gmm_k']
-            self.A = np.zeros((meta['n_dim'], meta['n_dim']))
+            self.A = np.zeros((len(meta['states']), len(meta['states'])))
             self.B = []
             for s in self.states:
                 gmm = GaussianMixtureModel(dim=meta['n_dim'], k=self.gmm_k)
